33253c035d2bf8e634674a5e8be4d737c9796aba
seiyanaru <seiyanaru@gmail.com>
2025-05-26 16:36:46 +0900

    new file:   EEG_analysis/Select_net/DE_3D_Feature.py 	new file:   EEG_analysis/Select_net/DE_3D_Feature_3freq.py 	new file:   EEG_analysis/Select_net/DE_4D_Feature.py 	new file:   EEG_analysis/Select_net/DE_4D_Feature_3freq.py 	new file:   EEG_analysis/Select_net/dataloader.py 	new file:   EEG_analysis/Select_net/dataloader_3freq_3d.py 	new file:   EEG_analysis/Select_net/toolbox.py

---
+++ EEG_analysis/Select_net/dataloader_3freq_3d.py
@@ -0,0 +1,36 @@
+import torch
+import numpy as np
+from toolbox import myDataset_5cv
+
+'''
+file name: DE_4D_Feature
+input: 1. choose nth fold of 5-fold
+       2. batch_size
+       3. random seed
+       4. the 4D feature of all subjects
+       
+output: train_dataloader and test_dataloader of nth fold
+'''
+
+#  choose nth fold
+n = 0
+# batch_size
+batch_size = 150
+# 随机数种子 random seed
+seed = 20
+# 使用gpu
+device = torch.device(("cuda:0") if torch.cuda.is_available() else "cpu")
+
+### 1.数据集 ### Dataset
+data = np.load('/content/drive/MyDrive/ColabNotebooks/SEED-VIGfile/Toshi_net/processedData/data_3d_3freq_flat.npy')   # 20355  
+label = np.load('./processedData/label.npy')
+
+dataG = torch.FloatTensor(data)
+labelG = torch.FloatTensor(label)
+
+print(dataG.shape)
+print(labelG.shape)
+#print(label)
+
+# 五折交叉验证 5-fold cross validation
+train_dataloader, test_dataloader = myDataset_5cv(dataG, labelG, batch_size, n, seed)
